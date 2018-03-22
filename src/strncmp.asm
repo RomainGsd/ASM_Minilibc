d7bc21fea2e69d7c21706ad0cd2c9101663ae3a4
Romain <romain.goasdoue@epitech.eu>
2018-03-22 15:33:44 +0100

    strncmp

---
+++ src/strncmp.asm
@@ -0,0 +1,35 @@
+[bits 64]
+
+        ; int strncmp(const char *str1, const char *str2, size_t n)
+
+section .text
+global strcmp
+
+strcmp:
+        push rbp
+        mov rbp, rsp
+
+        xor rcx, rcx
+
+strcmpLoop:
+	cmp rdx, rcx
+	jz exit
+        mov r10b, [rdi + rcx]
+        mov r8b, [rsi + rcx]
+	cmp r10b, 0
+        je exit
+        cmp r8b, 0
+        je exit
+        cmp r10b, r8b
+        jne exit
+        inc rcx
+	
+        jmp strcmpLoop
+
+exit:
+        sub r10b, r8b           ; diff to return
+        movsx rax, r10b		; movsx to put 8bit register into 64 bit register
+
+        mov rsp, rbp
+        pop rbp
+        ret
