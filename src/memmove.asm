728ad830b8d69b25a5277e8f41648a6f7c08be88
Romain <romain.goasdoue@epitech.eu>
2018-03-22 15:23:16 +0100

    strcmp

---
+++ src/memmove.asm
@@ -0,0 +1,20 @@
+[bits 64]
+
+	; void *memmove(void *str1, const void *str2, size_t n)
+
+section .text
+global memmove
+
+memmove:
+	push rbp
+	mov rbp, rsp
+
+	xor rcx, rcx
+
+memmoveLoop:
+	
+
+exit:
+	mov rsp, rbp
+	pop rbp
+	ret
--- src/strcmp.asm
+++ src/strcmp.asm
@@ -12,9 +12,21 @@ strcmp:
 	xor rcx, rcx
 	
 strcmpLoop:
-	
+	mov r10b, [rdi + rcx]
+	mov r8b, [rsi + rcx]
+	cmp r10b, 0
+	je exit
+	cmp r8b, 0
+	je exit
+	cmp r10b, r8b
+	jne exit
+	inc rcx
+	jmp strcmpLoop
 
 exit:
+	sub r10b, r8b 		; diff to return
+	movsx rax, r10b		; movsx to put 8bit register into 64 bit register
+	
 	mov rsp, rbp
         pop rbp
         ret
