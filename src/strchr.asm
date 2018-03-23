33eeb17359e865be9d0eb76713b868b22d57ca93
Romain <romain.goasdoue@epitech.eu>
2018-03-23 14:58:30 +0100

    strchr

--- src/strchr.asm
+++ src/strchr.asm
@@ -1,14 +1,18 @@
 [BITS 64]
-	
+
 section .text
 global strchr		; char *strchr(const char *, int c)
-	
+
 strchr:
 	push rbp
 	mov rbp, rsp
 
 	xor rcx, rcx
-	
+
+strLen:
+	cmp sil, 0
+	jz retNull
+
 strchrLoop:
 	cmp byte [rdi + rcx], sil
 	je retVal
@@ -19,13 +23,13 @@ strchrLoop:
 
 retVal:
 	mov rax, rdi
+	add rax, rcx
 	jmp exit
 
 retNull:
 	mov rax, 0
-	
+
 exit:
 	mov rsp, rbp
 	pop rbp
 	ret
-
